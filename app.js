d1e96b0b2014fc016bdd7e3191a9abb75666ffe0
Bao Lam <lamtantambao1989@yahoo.com>
2022-09-09 15:27:46 -0300

    Complete Exercise

---
+++ app.js
@@ -0,0 +1,9 @@
+let names = ['Gabriel', 'Sebastien', 'Keenan'];
+
+for (const name of names) {
+	console.log(name);
+}
+
+console.log(process.version);
+console.log(process.platform);
+console.log(process.pid);
